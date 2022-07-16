38d8b9f9b18379f10a8b4e4a7e78e0239bde796a
vaidyasaivarun22 <vaisyasaivarun22@gmail.com>
2022-07-16 20:07:04 +0530

    added scroll progress bar

--- code.js
+++ code.js
@@ -341,7 +341,7 @@ const button1 = document.getElementById('show');
     };    
 })(); 
 
-// When the user scrolls the page, execute myFunction
+// When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
